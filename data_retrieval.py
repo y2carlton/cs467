547fb5c6cb20e086e1ab84c0618d7d7bc8e1d964
Allen <allenjhuang92@gmail.com>
2022-02-06 11:25:18 -0600

    Change line endings to CRLF instead of LF

--- data_retrieval.py
+++ data_retrieval.py
@@ -5,6 +5,18 @@ import pickle
 import requests
 
 
+# NOTE: Only use below function if in QuantConnect platform.
+# def get_history_qb(
+#     qb: QuantBook,
+#     symbol_str: str,
+#     start: datetime.datetime,
+#     end: datetime.datetime,
+# ) -> pd.core.frame.DataFrame:
+#     """Returns price and volume history for the symbol."""
+#     symbol = qb.AddEquity(symbol_str, Resolution.Daily).Symbol
+#     return qb.History(symbol, start, end).loc[symbol]
+
+
 def get_pcr(kind: str, symbol_str: str, day_range: int) -> str:
     """Returns the put-call ratio for the symbol.
 
