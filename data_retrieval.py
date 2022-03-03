a6305a9bfad1c3d81c15c56fd57ebf776d8c2669
Allen <allenjhuang92@gmail.com>
2022-03-03 15:55:52 -0600

    Updated imports and type annotation style

--- data_retrieval.py
+++ data_retrieval.py
@@ -8,37 +8,47 @@ import yfinance as yf
 
 import datetime
 import os
-import pickle
-
-
-# NOTE: Only use below function if in QuantConnect platform.
-# def get_history_qb(
-#     qb: QuantBook,
-#     symbol_str: str,
-#     start: datetime.datetime,
-#     end: datetime.datetime,
-# ) -> pd.core.frame.DataFrame:
-#     """Returns price and volume history for the symbol."""
-#     symbol = qb.AddEquity(symbol_str, Resolution.Daily).Symbol
-#     return qb.History(symbol, start, end).loc[symbol]
 
 
 def get_history_yf(
-    symbol: str,
-    interval: str = "5m",
-    period: str = "60d",
-    auto_adjust: bool = False,
-) -> pd.core.frame.DataFrame:
-    """Returns a pandas DataFrame containing the symbol's historical data."""
+    symbol,
+    interval="5m",
+    period="60d",
+    auto_adjust=False,
+):
+    """Returns a pandas DataFrame containing the symbol's historical data.
+
+    Args:
+        symbol (str)
+        interval (str)
+        period (str)
+        auto_adjust (bool)
+
+    Returns:
+        pd.core.frame.DataFrame
+    """
     return yf.download(symbol, interval=interval, period=period, auto_adjust=False)
 
 
 def get_history_apca(
-    symbol: str,
-    interval: TimeFrame = TimeFrame(5, TimeFrameUnit.Minute),
-    adjustment: str = "raw",
-) -> pd.core.frame.DataFrame:
-    """Returns a pandas DataFrame containing the symbol's historical data."""
+    symbol,
+    interval=TimeFrame(5, TimeFrameUnit.Minute),
+    start=None,
+    end=None,
+    adjustment="raw",
+):
+    """Returns a pandas DataFrame containing the symbol's historical data.
+
+    Args:
+        symbol (str)
+        interval (alpaca_trade_api.rest.TimeFrame)
+        start (str)
+        end (str)
+        adjustment (str)
+
+    Returns:
+        pd.core.frame.DataFrame
+    """
 
     # Check for API key in environment variable, load from .env file if possible.
     var_name = "APCA_API_KEY_ID"
@@ -57,36 +67,45 @@ def get_history_apca(
             )
     api = REST()
 
-    # NOTE: Cannot get data from the past 15 minutes, so get data up to yesterday.
-    earliest = (datetime.datetime(year=1600, month=1, day=1)).strftime("%Y-%m-%d")
-    yesterday = (datetime.datetime.today() - relativedelta(days=1)).strftime("%Y-%m-%d")
+    if start is None:
+        start = (datetime.datetime(year=1600, month=1, day=1)).strftime("%Y-%m-%d")
+    if end is None:
+        # NOTE: Cannot get data from the past 15 minutes, so get data up to yesterday.
+        end = (datetime.datetime.today() - relativedelta(days=1)).strftime("%Y-%m-%d")
 
     return api.get_bars(
         symbol,
         interval,
-        earliest,
-        yesterday,
+        start,
+        end,
         adjustment=adjustment,
     ).df
 
 
-def get_pcr(kind: str, symbol: str, day_range: int) -> str:
+def get_pcr(kind, symbol, day_range):
     """Returns the put-call ratio for the symbol.
 
-    Valid kinds: 'volume', 'vol', 'open_interest', 'openinterest', 'oi'
-    Example symbol: 'VTI'
-    Valid day_ranges: 10, 20, 30, 60, 90, 120, 180
+    Args:
+        kind (str): 'volume', 'vol', 'open_interest', 'openinterest', 'oi'
+        symbol (str)
+        day_ranges (int): 10, 20, 30, 60, 90, 120, 180
+
+    Returns:
+        str
     """
     url = f"https://mktdata.fly.dev/pcr/{kind}/{symbol}/{day_range}"
     pcr_str = requests.get(url).text
     return pcr_str
 
 
-def get_er(symbol: str) -> str:
+def get_er(symbol):
     """Returns the expense ratio for the symbol.
 
-    Example symbol: 'TQQQ'
-    symbol is case sensitive.
+    Args:
+        symbol (str): Case sensitive
+
+    Returns:
+        str
     """
     url = f"https://mktdata.fly.dev/er/{symbol}"
     er_str = requests.get(url).text
@@ -94,7 +113,11 @@ def get_er(symbol: str) -> str:
 
 
 def get_fgi() -> dict:
-    """Returns the current CNN fear and greed index."""
+    """Returns the current CNN fear and greed index.
+
+    Returns:
+        dict
+    """
     url = f"https://mktdata.fly.dev/fgi"
     response_json = requests.get(url).json
     return response_json
